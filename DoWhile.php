d890ace6c4f0e1fc8794af77e17d150e77fb34cd
Antonio Nascimento <aeduardoamaral@gmail.com>
2020-01-13 12:12:06 -0300

    Add files via upload

---
+++ DoWhile.php
@@ -0,0 +1,37 @@
+<?php
+echo"Loop Do while<br/><br/>";
+
+$i=1;
+$tam=15;
+
+do
+{
+    echo"</pre>Variavel -> i <- tem o valor : $i<br/>";
+    echo"Variavel -> tam <- tem o valor :  $tam</pre><br/>";
+    $i++;
+}
+  
+while($i<$tam);
+
+
+echo"<br/><hr/><br/>";
+echo"<br/><hr/><br/>";
+
+echo"Loop while<br/><br/>";
+
+$i=1;
+$tam=15;
+
+
+while($i<$tam)
+
+{
+    echo "<pre> Variável i tem o valor  ;  $i<br/>";
+    echo "Variável tam tem o valor:    $tam</pre><br/>";
+    $i++;
+}
+
+
+
+
+?>
